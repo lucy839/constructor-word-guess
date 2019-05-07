6c0f7bb5667e8e2b24c6034be13b630659fc42ff
Youngju Lee <lucylee839@hotmail.com>
2019-05-07 09:51:55 -0700

    Added letter constructor

---
+++ Letter.js
@@ -0,0 +1,32 @@
+function Letter(letter) {
+    // A string value to store the underlying character for the letter
+    this.letter = letter;
+
+    // A boolean value that stores whether that letter has been guessed yet
+    this.isCorrect = false;
+
+    // A function that returns the underlying character if the letter has been guessed, 
+    // or a placeholder (like an underscore) if the letter has not been guessed
+    this.getLetter = function() {
+        if (!this.isCorrect) {
+            return "_";
+        } else {
+            return this.letter;
+        }
+    }
+
+    // A function that takes a character as an argument and checks it against the underlying character,
+    //  updating the stored boolean value to true if it was guessed correctly
+    this.checkLetter = function(guess) {
+        if (guess === this.letter) {
+            this.isCorrect = true;
+        }
+    }
+}
+
+module.exports = Letter;
+
+
+
+
+
---
+++ Word.js
@@ -0,0 +1,26 @@
+// Contains a constructor, Word that depends on the Letter constructor. 
+const Letter = require("./Letter.js");
+
+function Word (word){
+    // An array of `new` Letter objects representing the letters of the underlying word
+    this.letters = [];
+    // A function that returns a string representing the word. 
+    // This should call the function on each letter object (the first function defined in `Letter.js`) 
+    // that displays the character or an underscore and concatenate those together.
+    this.getWord = function(word){
+        var wordArr = this.word.split("");
+        for (var i in wordArr){
+            var newLetter = new Letter (wordArr[i])
+            this.letters.push(newLetter)
+        }
+    }
+    // A function that takes a character as an argument and calls the guess function 
+    // on each letter object (the second function defined in `Letter.js`)
+    this.checkWord = function(guess){
+        this.letters.forEach(letter => {
+            
+        })
+        this.checkLetter(guess);
+    }
+}
+module.exports = Word;
